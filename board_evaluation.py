2b8feb97eb4921ea80b745194a5319339ce8e00f
Daniel Stoi <46283690+mrbunton@users.noreply.github.com>
2020-01-03 05:20:45 -0500

    Update board_evaluation.py

--- board_evaluation.py
+++ board_evaluation.py
@@ -1,3 +1,14 @@
+from ai2048 import e_effective_link as link,e_power_corner as corner,e_power_corner_stability as stab,score
+#link,corner,stab
 def function(subtree):
-    from ai import basic_fct2
-    basic_fct2(subtree)
+    board = subtree.cur.board
+    ans = 0
+    for y in range(4):
+        for x in range(4):
+            ans+=board[y][x]*link(board,[y,x])
+    
+    ans+=score(board)
+    k=corner(board)
+    ans+=k[0]*k[1]
+    
+    return int(ans)
