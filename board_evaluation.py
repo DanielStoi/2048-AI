e550f26245943b60812840483bb1e2ecd05b413f
Daniel Stoilescu <46283690+mrbunton@users.noreply.github.com>
2020-01-05 21:44:08 -0500

    Update board_evaluation.py

--- board_evaluation.py
+++ board_evaluation.py
@@ -1,4 +1,4 @@
-from ai2048 import e_effective_link as link,e_power_corner as corner,e_power_corner_stability as stab,score
+from ai2048 import e_effective_link as link,e_power_corner as corner,e_power_corner_stability as stab,score,entrapment
 #link,corner,stab
 def function(subtree):
     board = subtree.cur.board
@@ -9,9 +9,9 @@ def function(subtree):
     
     ans+=score(board)
     k=corner(board)
-    ans+=k[0]*k[1]**4
+    ans+=k[0]*k[1]
     for y in range(4):
         for x in range(4):
             ans+=board[y][x]*link(board,[x,y])
-    
+            ans-=entrampment(board,[x,y])
     return int(ans)
