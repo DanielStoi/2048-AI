928e6cc45ebb7ce2ffaac2bc53727c0790655e18
Daniel Stoilescu <46283690+DanielStoi@users.noreply.github.com>
2021-07-24 22:22:47 +1000

    Update classifier_builder.py

--- classifier_builder.py
+++ classifier_builder.py
@@ -2,6 +2,7 @@ from ai2048 import e_effective_link as link,e_power_corner as corner,e_power_cor
 #link,entrapment, conrner,stab,score, 
 from game2048 import Game
 import ai2048 as ai
+from board_evaluation import function as fct2
 
 class classifier:
     weights = []
@@ -33,17 +34,30 @@ class classifier:
     def set(self, scale):
         scale = self
 
-    def simulate_effectivness(n): #runs 2024 game and saves score, n times
-        ans = 0
-        for _ in range(n):
-            game = Game()
-            game.spawn_tile()
-            game.spawn_tile()
-            cont = True
-            while cont:
-                function = ai.find_move(game,)
-                
-            
-            
-        
-        
+
+
+def evaluate_effectivness(n,fct): #runs 2024 game and saves score, n times
+    ans = 0
+    for _ in range(n):
+        game = Game()
+        game.spawn_tile()
+        game.spawn_tile()
+        cont = True
+        while cont:
+            k = ai.find_move(game,3,fct,fct2,1)
+            if k != -1:
+                if game.move(k):
+                    game.spawn_tile()      
+            if k == 0:
+                ans+= ai.score(game.board)
+                break
+    return ans/n
+
+def evaluate_expected_time():
+    a = int(input("what is the expected amount of itterations you want to train the classifier: "))
+    b = int(input("how much iterations of the the game should be run for each classifier to be evaluated: "))
+    print("expected time is "+str(a*b)+" minutes, or "+str(a*b//60)+" hours.")
+
+
+if __name__ == '__main__':
+    evaluate_expected_time()
