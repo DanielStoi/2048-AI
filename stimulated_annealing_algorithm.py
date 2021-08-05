e5f01ee9ff781891089ed5ea349a1358a1edfff9
Daniel Stoilescu <46283690+DanielStoi@users.noreply.github.com>
2021-08-05 22:47:43 +1000

    Update stimulated_annealing_algorithm.py

--- stimulated_annealing_algorithm.py
+++ stimulated_annealing_algorithm.py
@@ -1,6 +1,6 @@
 from random import randint
 from classifier_builder import *
-from math import exp
+from math import exp,log
 
 """
 idea:
@@ -8,7 +8,8 @@ run the game over and over again with some random modification on the weights
 keep the new weights if an impovement has been made, otherwise keep it with
 a certain probability
 """
-#randint(0,10))
+
+
 def intro_sim_annealing():
     print("simulated annealing is an iterative algorithm which aims to (in this case) tries to optimise the weights of the different classifiers in determining how good a position is")
     print("this algorithm will go through many iterations through the stimulated_annealing algorithm and returns the weights")
@@ -40,31 +41,39 @@ def stimulated_annealing(T,data):
     for i in range(1,T):
         print("current expected score: "+str(cur))
         new_weights = modify_weights_randomly(w,step)
+        cur = evaluate_effectivness(alg.function,y)
         alg.weights = new_weights
         eff = evaluate_effectivness(alg.function,y)
         if cur <= eff:
             #print("chose new weight due to it being better")
             w = new_weights
             cur = eff
+            print("accepted better scoring weighs")
         else:
-            p = exp((cur-eff)/(i*10000))
-            if randint(0,int(p*1000))>500:
-                #print("chose new weight due to probability")
-                w = new_weights
-                cur = eff
-            else:
-                pass
-                #print("rejected new weight")
+            try:
+                
+                p = exp((log(eff)-log(cur))*i/1000)
+                if randint(0,int(p*100))-data["prob"] > 10+ i*1.2:
+                    #print("chose new weight due to probability")
+                    w = new_weights
+                    cur = eff
+                    print("randommly accepted worse scoring weighs")
+                else:
+                    print("rejected new weight")
+                    pass
+            except:
+                print("Overflow occured")
     print("weights: ")
     print(" ".join(map(str,w)))
     
-    print("expected score: "+ str(evaluate_effectivness(alg.function,y*5)))
+    print("expected score: "+ str(evaluate_effectivness(alg.function,10)))
     return w
                 
             
-print()           
-data = {"initial_range":100, "max_step":5, "evaluation_strength":1}
+         
+data = {"initial_range":100, "max_step":100, "evaluation_strength":10}
 iterations = int(input("how many iterations of training: "))
-print("expected time in hours: " + str(20*iterations/(60*60)))
+data["evaluation_strength"] = int(input("how many iterations of each classifier should be run in order to gauge their strenghts: "))
+data["prob"] = int(input("rareness of switching to lower score probability (integer value divisor): "))
+print("expected time in hours: " + str(20*iterations*data["evaluation_strength"]/(60*60)))
 stimulated_annealing(iterations,data)
-
